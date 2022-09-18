dea09de680fd3a6524b2c75e1654ce71bd09a839
Manteiga <rafael.manteiga@hotmail.com>
2022-09-18 05:43:22 -0300

    Update bsudoku.py

--- bsudoku.py
+++ bsudoku.py
@@ -2,10 +2,11 @@ def testafimhorizontal(jogo):
     soma = 0
     for k in range(1,3):
         for i in range(1,3):
+            soma = soma+jogo[k][i]
             if jogo[k][i]==jogo[k-1][i-1]:
-                # jogo continua
-            else:
-                # fim de jogo
+                continue
+            elif soma==45:
+                return True
                 
 def testafimvertical(jogo1,jogo2,jogo3):
     
