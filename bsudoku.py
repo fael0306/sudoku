aa4e57390444c20eb154f9c0fed72c959ed39133
Manteiga <rafael.manteiga@hotmail.com>
2022-09-18 04:25:39 -0300

    Add files via upload

---
+++ bsudoku.py
@@ -0,0 +1,11 @@
+def testafimhorizontal(jogo):
+    soma = 0
+    for k in range(1,3):
+        for i in range(1,3):
+            if jogo[k][i]==jogo[k-1][i-1]:
+                # jogo continua
+            else:
+                # fim de jogo
+                
+def testafimvertical(jogo1,jogo2,jogo3):
+    
