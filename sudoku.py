de96b2bff4f70a7639fc7d41afb07d8ad7bf41f7
Manteiga <rafael.manteiga@hotmail.com>
2022-09-21 13:53:18 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,21 @@
-jogo1 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo2 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo3 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo4 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo5 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo6 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo7 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo8 = [["_","_","_"],["_","_","_"],["_","_","_"]]
-jogo9 = [["_","_","_"],["_","_","_"],["_","_","_"]]
+jogo1 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo2 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo3 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo4 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo5 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo6 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo7 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo8 = [[0,0,0],[0,0,0],[0,0,0]]
+jogo9 = [[0,0,0],[0,0,0],[0,0,0]]
 
 for k in range(3):
     print(" ".join(jogo1[k])," ".join(jogo2[k])," ".join(jogo3[k]))
     print(" ".join(jogo4[k])," ".join(jogo5[k])," ".join(jogo6[k]))
     print(" ".join(jogo7[k])," ".join(jogo8[k])," ".join(jogo9[k]))
 
-# Só pode testar vitória quando todos os espaços estiverem preenchidos
+    
+#while !testehorizontal and !testevertical:
+    #pede entrada do jogador
+    #while entrada<1 or entrada>9:
+        #pede entrada do jogador novamente
+    
