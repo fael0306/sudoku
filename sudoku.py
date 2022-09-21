aeff609095c94d2bec6ce648ecdfcd65aca9de35
Manteiga <rafael.manteiga@hotmail.com>
2022-09-21 14:04:20 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,5 @@
+import bsudoku
+
 jogo1 = [[0,0,0],[0,0,0],[0,0,0]]
 jogo2 = [[0,0,0],[0,0,0],[0,0,0]]
 jogo3 = [[0,0,0],[0,0,0],[0,0,0]]
@@ -8,12 +10,8 @@ jogo7 = [[0,0,0],[0,0,0],[0,0,0]]
 jogo8 = [[0,0,0],[0,0,0],[0,0,0]]
 jogo9 = [[0,0,0],[0,0,0],[0,0,0]]
 
-for k in range(3):
-    print(" ".join(jogo1[k])," ".join(jogo2[k])," ".join(jogo3[k]))
-    print(" ".join(jogo4[k])," ".join(jogo5[k])," ".join(jogo6[k]))
-    print(" ".join(jogo7[k])," ".join(jogo8[k])," ".join(jogo9[k]))
-
-    
+bsudoku.mostrajogo()
+  
 #while !testehorizontal and !testevertical:
     #pede entrada do jogador
     #while entrada<1 or entrada>9:
