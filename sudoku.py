cc4701f0f8f9c9d56dbefe0a22ebc4fdd7f84bdc
Manteiga <rafael.manteiga@hotmail.com>
2022-09-23 20:00:33 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -11,7 +11,7 @@ jogo = [[0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0]]
 
 bsudoku.mostrajogo(jogo)
-#while bsudoku.testavitoria(jogo1,jogo2,jogo3,jogo4,jogo5,jogo6,jogo7,jogo8,jogo9)==False:
+#while bsudoku.testavitoria(jogo)==False:
 #    n = int(input("Digite o número: "))
 #    while n<1 or n>9:
 #        n = int(input("Digite um número válido: "))
@@ -21,24 +21,6 @@ bsudoku.mostrajogo(jogo)
 #    p2 = int(input("Digite a coluna: "))
 #    while p2<1 or p2>9:
 #        p2 = int(input("Digite uma coluna válida: "))
-# A forma de printar a matriz está errada. Erro está na função mostrarjogo.
-   # if p1==1:
-        
-    #if p1==2:
-        
-    #if p1==3:
-        
-    #if p1==4:
-        
-    #if p1==5:
-        
-    #if p1==6:
-        
-    #if p1==7:
-        
-    #if p1==8:
-        
-    #if p1==9:
-        
+#    jogo[p1-1][p2-1]=n   
 #    bsudoku.mostrajogo(jogo1,jogo2,jogo3,jogo4,jogo5,jogo6,jogo7,jogo8,jogo9)
-    
+#    print("")
