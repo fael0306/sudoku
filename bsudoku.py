217a2f0cf74c002b772dd135062c506ec0542d71
Manteiga <rafael.manteiga@hotmail.com>
2022-09-26 00:45:51 -0300

    Add files via upload

--- bsudoku.py
+++ bsudoku.py
@@ -23,6 +23,7 @@ def sorteiasudoku():
     lista = [sudoku1,sudoku2]#,sudoku3,sudoku4,sudoku5,sudoku6,sudoku7,sudoku8,sudoku9,sudoku10]
     return mostrajogo(random.choice(lista))
 
+# Não precisa testar se a soma dá 45. Se todos os números estão entre 1 e 9 e não repetem na vertical e horizontal, está certo o jogo.
 def testarepetidosh(jogo):
     for i in range(0,9):
         for j in range(0,8):
@@ -31,38 +32,25 @@ def testarepetidosh(jogo):
                     return False
     return True
     
-def testahorizontal(jogo):
-    soma = 0
-    if(testarepetidosh(jogo)):
-        for k in range(0,9):
-            for i in range(0,9):
-                soma = soma+jogo[k][i]
-            if soma!=45:
-                return False
-            soma = 0
-    else:
-        return False
-    return True
-
-def testarepetidosv(matriz):
+def testarepetidosv(jogo):
     for k in range(0,9):
         for i in range(0,8):
             for j in range(i+1,9):
                 if (jogo[j][k]==jogo[i][k]):
                     return False
     return True
-#def testavertical(jogo) - Verifica se a soma dá 45 em todas as verticais. Chama a função para testar repetidos antes, caso haja repetidos, nem roda a soma.
-#                          Vertical: repete a coluna e varia cada linha, depois varia a coluna e repete a linha... 
+
+
 #def testarepetidosq(jogo) - Verifica se cada elemento de cada quadrado 3x3 da matriz possui números diferentes
-#def testaquadrados(jogo) - Verifica se 00 01 02 10 11 12 20 21 22 são diferentes e somam 45
-#                                       03 04 05 13 14 15 23 24 25 são diferentes e somam 45
-#                                       06 07 08 16 17 18 26 27 28 são diferentes e somam 45
-#                                       30 31 32 40 41 42 50 51 52 são diferentes e somam 45
-#                                       33 34 35 43 44 52 53 54 55 são diferentes e somam 45
-#                                       36 37 38 46 47 48 56 57 58 são diferentes e somam 45
-#                                       60 61 62 70 71 72 80 81 82 são diferentes e somam 45
-#                                       63 64 65 73 74 75 83 84 85 são diferentes e somam 45
-#                                       66 67 68 76 77 78 86 87 88 são diferentes e somam 45
+#def testaquadrados(jogo) - Verifica se 00 01 02 10 11 12 20 21 22 são diferentes
+#                                       03 04 05 13 14 15 23 24 25 são diferentes
+#                                       06 07 08 16 17 18 26 27 28 são diferentes
+#                                       30 31 32 40 41 42 50 51 52 são diferentes
+#                                       33 34 35 43 44 52 53 54 55 são diferentes
+#                                       36 37 38 46 47 48 56 57 58 são diferentes
+#                                       60 61 62 70 71 72 80 81 82 são diferentes
+#                                       63 64 65 73 74 75 83 84 85 são diferentes
+#                                       66 67 68 76 77 78 86 87 88 são diferentes
 #                                       NECESSÁRIO ACHAR O PADRÃO DE REPETIÇÃO PARA TESTAR
 # O def de testar quadrado gira em torno de algo parecido com as repetições abaixo
 #soma = 0
