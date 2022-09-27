8699710a9265bc1f4868737332295a9ce72ef0fd
Manteiga <rafael.manteiga@hotmail.com>
2022-09-27 03:51:29 -0300

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -8,15 +8,19 @@ def main():
     imutaveis = bsudoku.verificapreenchidos(jogo)
 
     while bsudoku.testavitoria(jogo)==False:
-        n = int(input("Digite o número: "))
-        while n<1 or n>9:
-            n = int(input("Digite um número válido: "))
-        p1 = int(input("Digite a linha: "))
-        while p1<1 or p1>9:
-            p1 = int(input("Digite uma linha válida: "))
-        p2 = int(input("Digite a coluna: "))
-        while p2<1 or p2>9:
-            p2 = int(input("Digite uma coluna válida: "))
+        try:
+            n = int(input("Digite o número: "))
+            while n<1 or n>9:
+                n = int(input("Digite um número válido: "))
+            p1 = int(input("Digite a linha: "))
+            while p1<1 or p1>9:
+                p1 = int(input("Digite uma linha válida: "))
+            p2 = int(input("Digite a coluna: "))
+            while p2<1 or p2>9:
+                p2 = int(input("Digite uma coluna válida: "))
+        except ValueError:
+            print("Você digitou algo que não é um número inteiro. O jogo será resetado.")
+            main()
         if jogo[p1-1][p2-1]==0:
             jogo[p1-1][p2-1]=n
             bsudoku.mostrajogo(jogo)
