d20460c2aa80c19be18e1d514c59e7a764edfae4
Manteiga <rafael.manteiga@hotmail.com>
2022-09-27 01:41:23 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,29 +1,32 @@
 import bsudoku
 
-jogo = bsudoku.sorteiasudoku()
+def main():
 
-bsudoku.mostrajogo(jogo)
-print("")
-imutaveis = bsudoku.verificapreenchidos(jogo)
+    jogo = bsudoku.sorteiasudoku()
 
-while bsudoku.testavitoria(jogo)==False:
-    n = int(input("Digite o número: "))
-    while n<1 or n>9:
-        n = int(input("Digite um número válido: "))
-    p1 = int(input("Digite a linha: "))
-    while p1<1 or p1>9:
-        p1 = int(input("Digite uma linha válida: "))
-    p2 = int(input("Digite a coluna: "))
-    while p2<1 or p2>9:
-        p2 = int(input("Digite uma coluna válida: "))
-    if jogo[p1-1][p2-1]==0:
-        jogo[p1-1][p2-1]=n
-    else: # Testar esse else
-        for k in (0,len(imutaveis)):
-            if imutaveis[k]==jogo[p1-1][p2-1]:
-                print("Essa posição está automaticamente preenchida. Escolha outra.")
-                bsudoku.mostrajogo(jogo)
-            else:
-                jogo[p1-1][p2-1]=n
     bsudoku.mostrajogo(jogo)
     print("")
+    imutaveis = bsudoku.verificapreenchidos(jogo)
+
+    while bsudoku.testavitoria(jogo)==False:
+        n = int(input("Digite o número: "))
+        while n<1 or n>9:
+            n = int(input("Digite um número válido: "))
+        p1 = int(input("Digite a linha: "))
+        while p1<1 or p1>9:
+            p1 = int(input("Digite uma linha válida: "))
+        p2 = int(input("Digite a coluna: "))
+        while p2<1 or p2>9:
+            p2 = int(input("Digite uma coluna válida: "))
+        if jogo[p1-1][p2-1]==0:
+            jogo[p1-1][p2-1]=n
+        else: # Testar esse else
+            for k in (0,len(imutaveis)):
+                if imutaveis[k]==jogo[p1-1][p2-1]:
+                    print("Essa posição está automaticamente preenchida. Escolha outra.")
+                    bsudoku.mostrajogo(jogo)
+                else:
+                    jogo[p1-1][p2-1]=n
+        bsudoku.mostrajogo(jogo)
+        print("")     
+main()
