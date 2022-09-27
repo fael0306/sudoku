2b7fa0dfed8385c403722b789e4079fb115e4ca8
Manteiga <rafael.manteiga@hotmail.com>
2022-09-27 05:54:44 -0300

    Update bsudoku.py

--- bsudoku.py
+++ bsudoku.py
@@ -159,7 +159,7 @@ def testarepetidosq(jogo):
     return True
 
 def testavitoria(jogo):
-    return(testarepetidosv==False or testarepetidosh==False or testarepetidosq==False)
+    return(testarepetidosv==True and testarepetidosh==True and testarepetidosq==True)
 
 def mostrajogo(jogo):
     a = ""
